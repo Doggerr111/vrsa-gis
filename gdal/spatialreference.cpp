9831448637f24d23d62bc80eb1cbd8c7dc81de40
Doggerr111 <doggerr111@gmail.com>
2025-11-11 22:53:04 +0300

    change constructor and add fabric methods

--- gdal/spatialreference.cpp
+++ gdal/spatialreference.cpp
@@ -1,13 +1,64 @@
 #include "spatialreference.h"
 
-vrsa::gdalwrapper::SpatialReference::SpatialReference(std::string wkt)
+vrsa::gdalwrapper::SpatialReference::SpatialReference()
+    :mCrs(new OGRSpatialReference)
+{
+
+}
+
+vrsa::gdalwrapper::SpatialReference::SpatialReference(std::string crsDefinition, common::CrsFormat format)
     : mCrs(new OGRSpatialReference)
 {
-    auto result = mCrs->importFromWkt(wkt.c_str());
-    if (result != OGRERR_NONE)
+    switch (format)
     {
-        VRSA_ERROR("SpatialReference", "Failed to import from WKT: " +wkt + "GDAL Error:" + std::to_string(result));
+
+    case common::CrsFormat::WKT:
+    {
+        auto result = mCrs->importFromWkt(crsDefinition.c_str());
+        if (result != OGRERR_NONE)
+        {
+            VRSA_ERROR("SpatialReference", "Failed to import from WKT: " +crsDefinition + "GDAL Error:" + std::to_string(result));
+        }
+        break;
     }
+
+    case common::CrsFormat::Proj:
+    {
+        auto result = mCrs->importFromProj4(crsDefinition.c_str());
+        if (result != OGRERR_NONE)
+        {
+            VRSA_ERROR("SpatialReference", "Failed to import from Proj: " +crsDefinition + "GDAL Error:" + std::to_string(result));
+        }
+        break;
+    }
+
+    case common::CrsFormat::EPSG:
+    {
+        try
+        {
+            int number = std::stoi(crsDefinition);
+            auto result = mCrs->importFromEPSG(number);
+            if (result != OGRERR_NONE)
+            {
+                VRSA_ERROR("SpatialReference", "Failed to import from EPSG: " + crsDefinition + " GDAL Error:" + std::to_string(result));
+            }
+        }
+        catch (const std::invalid_argument& e)
+        {
+            VRSA_ERROR("SpatialReference", "Failed to import from EPSG (invalid argument): " + crsDefinition);
+        }
+        catch (const std::out_of_range& e)
+        {
+            VRSA_ERROR("SpatialReference", "Failed to import from EPSG (out of range): " + crsDefinition);
+        }
+        catch (const std::exception& e)
+        {
+            VRSA_ERROR("SpatialReference", "Failed to import from EPSG: " + std::string(e.what()));
+        }
+        break;
+    }
+    }
+
 }
 
 
@@ -37,3 +88,9 @@ vrsa::gdalwrapper::SpatialReference &vrsa::gdalwrapper::SpatialReference::operat
     }
     return *this;
 }
+
+vrsa::gdalwrapper::SpatialReference vrsa::gdalwrapper::SpatialReference::fromWKT(const std::string &wkt)
+{
+    //return SpatialReference(wkt);
+}
+
--- gdal/spatialreference.h
+++ gdal/spatialreference.h
@@ -1,7 +1,7 @@
 #ifndef SPATIALREFERENCE_H
 #define SPATIALREFERENCE_H
 #include "gdal/gdalresourcehandles.h"
-
+#include "common/GisDefines.h"
 namespace vrsa
 {
 namespace gdalwrapper
@@ -10,16 +10,29 @@ namespace gdalwrapper
 class SpatialReference
 {
 public:
-    SpatialReference(std::string wkt);
+    SpatialReference();
+    SpatialReference(std::string str, vrsa::common::CrsFormat format);
     SpatialReference(int epsgCode);
     SpatialReference(const SpatialReference& other);
     SpatialReference& operator=(const SpatialReference& other);
+
+
+    static SpatialReference fromWKT(const std::string& wkt);
+    static SpatialReference fromEPSG(int epsgCode);
+    static SpatialReference fromProj(const std::string& projString);
+    static SpatialReference fromUTM(int zone, bool northernHemisphere = true);
+
+    inline const gdalwrapper::OgrSpatialRefPtr& GetOGRSpatialRef() const noexcept
+    {
+        return mCrs;
+    }
 
     SpatialReference(SpatialReference&& other) noexcept = default;
     SpatialReference& operator=(SpatialReference&& other) noexcept = default;
 
 private:
     vrsa::gdalwrapper::OgrSpatialRefPtr mCrs;
+
 };
 
 }
