bd7ad93b698db97bd62973b81a96c5ea458f2345
Doggerr111 <doggerr111@gmail.com>
2025-11-16 20:46:46 +0300

    add class for unit conversion

---
+++ calculations/unitconverter.h
@@ -0,0 +1,33 @@
+#ifndef UNITCONVERTER_H
+#define UNITCONVERTER_H
+#include <QGuiApplication>
+#include <QScreen>
+#include <QThread>
+#include <QtConcurrent/QtConcurrent>
+#include <QMutex>
+#include <QFuture>
+namespace vrsa
+{
+namespace calculations
+{
+
+
+class UnitConversion {
+public:
+    static double mmToPixels(double mm, double dpi = 96.0)
+    {
+        const double dots_per_millimeter = (QGuiApplication::primaryScreen()->physicalDotsPerInch() / 25.40);
+        return mm*dots_per_millimeter;
+    };
+    static double pixelsToMm(double pixels, double dpi = 96.0);
+    static double mmToScenePixels(double mm, double dpi = 96.0, double sceneScale = 1.0);
+
+    static constexpr double STANDARD_DPI = 96.0;
+};
+
+}}
+
+
+
+
+#endif // UNITCONVERTER_H
