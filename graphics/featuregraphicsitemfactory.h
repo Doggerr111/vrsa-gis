519b706f3e9262035104fcbed6f49dad841b480f
Doggerr111 <doggerr111@gmail.com>
2026-02-20 08:48:46 +0300

    Change factory method for creating graphic items

--- graphics/featuregraphicsitemfactory.h
+++ graphics/featuregraphicsitemfactory.h
@@ -21,10 +21,10 @@ public:
 
 public:
     static std::unique_ptr<FeatureGraphicsItem> createForFeature(
-            const vrsa::vector::VectorFeature* feature, vrsa::graphics::VectorFeatureStyle style)
+            const vrsa::vector::VectorFeature* feature, vrsa::graphics::VectorFeatureStyle* style)
     {
-        auto strategy = createPainterPolicy(feature, style);
-        auto item = std::make_unique<FeatureGraphicsItem>(std::move(strategy), feature);
+        auto renderer = std::make_unique<FeatureGraphicsItemRenderer>(style, feature->getType());
+        auto item = std::make_unique<FeatureGraphicsItem>(std::move(renderer), feature);
         QObject::connect(feature, &vector::VectorFeature::visibilityChanged,
                          item.get(), &FeatureGraphicsItem::setVisible);
         return item;
@@ -33,27 +33,27 @@ public:
 
     //static std::unique_ptr
 private:
-    static std::unique_ptr<VectorFeatureDrawingPolicy> createPainterPolicy(
-            const vrsa::vector::VectorFeature* feature,
-            VectorFeatureStyle& style
-        ) {
-            using namespace vrsa::common;
-            switch (feature->getType()) {
-            case GeometryType::Point:
-                return std::make_unique<PointFeatureDrawingPolicy>(style);
-            case GeometryType::MultiPoint:
-                return std::make_unique<MultiPointFeatureDrawingPolicy>(style);
-            case GeometryType::LineString:
-                return std::make_unique<LineFeatureDrawingPolicy>(style);
-            case GeometryType::MultiLineString:
-                return std::make_unique<MultiLineFeatureDrawingPolicy>(style);
-            case GeometryType::Polygon:
-                return std::make_unique<PolygonFeatureDrawingPolicy>(style);
-            case GeometryType::MultiPolygon:
-                return std::make_unique<MultiPolygonFeatureDrawingPolicy>(style);
-            }
-            //return std::make_unique<UnknownGeometryPainterStrategy>(style);
-        }
+//    static std::unique_ptr<VectorFeatureDrawingPolicy> createPainterPolicy(
+//            const vrsa::vector::VectorFeature* feature,
+//            VectorFeatureStyle& style
+//        ) {
+//            using namespace vrsa::common;
+//            switch (feature->getType()) {
+//            case GeometryType::Point:
+//                return std::make_unique<PointFeatureDrawingPolicy>(style);
+//            case GeometryType::MultiPoint:
+//                return std::make_unique<MultiPointFeatureDrawingPolicy>(style);
+//            case GeometryType::LineString:
+//                return std::make_unique<LineFeatureDrawingPolicy>(style);
+//            case GeometryType::MultiLineString:
+//                return std::make_unique<MultiLineFeatureDrawingPolicy>(style);
+//            case GeometryType::Polygon:
+//                return std::make_unique<PolygonFeatureDrawingPolicy>(style);
+//            case GeometryType::MultiPolygon:
+//                return std::make_unique<MultiPolygonFeatureDrawingPolicy>(style);
+//            }
+
+//        }
 };
 }
 }
