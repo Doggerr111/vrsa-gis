ff4e03d42d128687bea78fadb815d67fcbf33cf2
Doggerr111 <doggerr111@gmail.com>
2026-03-15 08:44:47 +0300

    ui: add dark theme QSS - stylesheet loaded in main.cpp on startup

--- main.cpp
+++ main.cpp
@@ -11,35 +11,32 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile file(":/app_styles/dark.qss");
+    if (file.open(QFile::ReadOnly))
+    {
+        QString styleSheet = QString::fromUtf8(file.readAll());
+        a.setStyleSheet(styleSheet);
+        file.close();
+        VRSA_INFO("UI", "Таблица стилей успешно загружена");
+    }
+    else
+        VRSA_ERROR("UI", "Таблица стилей не загружена. Путь:" + QFileInfo(file).absoluteFilePath().toStdString());
 
-
-    qDebug()<<QRandomGenerator::global()->bounded(0, 100);
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
+    for (const QString &locale : uiLanguages)
+    {
         const QString baseName = "VRSA_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
+        if (translator.load(":/i18n/" + baseName))
+        {
             a.installTranslator(&translator);
             break;
         }
     }
 
     GDALAllRegister();
-    std::cout << "GDAL works!" << std::endl;
     vrsa::common::Logger::getInstance().setLogFile(QDir::currentPath().toStdString()+"/application.log");
     vrsa::common::Logger::getInstance().setLogLevel(vrsa::common::LogLevel::TRACE);
-//    VRSA_DEBUG("GDAL", "Opening file");
-    //VRSA_LOG_MEMORY("Start");
-
-//    std::vector<int*> arrays;
-//    for(int i = 0; i < 10; i++) {
-//        arrays.push_back(new int[1000000]);  // 4 MB каждый
-//        for(int j = 0; j < 1000000; j++) arrays[i][j] = j;
-//        VRSA_LOG_MEMORY("After allocation " + std::to_string(i));
-//    }
-
-    //return 0;
-
 
     qRegisterMetaTypeStreamOperators<vrsa::common::TreeItemType>("vrsa::common::CustomRoles");
     qRegisterMetaTypeStreamOperators<vrsa::common::TreeItemType>("vrsa::common::GeometryType");
