9c9aa495fdcf70bf02466c307978220818315b73
Doggerr111 <doggerr111@gmail.com>
2026-03-24 11:53:00 +0300

    feat: add custom comboboxes for raster resampling and compression methods

---
+++ customWidgets/rasterProccessingWidgets/resamplingmethodcombobox.h
@@ -0,0 +1,12 @@
+#ifndef RESAMPLINGMETHODCOMBOBOX_H
+#define RESAMPLINGMETHODCOMBOBOX_H
+
+#include <QComboBox>
+
+class ResamplingMethodComboBox : public QComboBox
+{
+public:
+    ResamplingMethodComboBox(QWidget* parent = nullptr);
+};
+
+#endif // RESAMPLINGMETHODCOMBOBOX_H
