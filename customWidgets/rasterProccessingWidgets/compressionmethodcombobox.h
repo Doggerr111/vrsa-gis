7a6884e98744a3123de609394e6eaa2b8078ed71
Doggerr111 <doggerr111@gmail.com>
2026-03-24 13:57:50 +0300

    docs: add doxygen comments

--- customWidgets/rasterProccessingWidgets/compressionmethodcombobox.h
+++ customWidgets/rasterProccessingWidgets/compressionmethodcombobox.h
@@ -1,8 +1,13 @@
 #ifndef COMPRESSIONMETHODCOMBOBOX_H
 #define COMPRESSIONMETHODCOMBOBOX_H
-
 #include <QComboBox>
 
+/**
+ * @brief Комбобокс для выбора метода сжатия растров
+ *
+ * @see vrsa::common::GeoTiffCompression
+ *
+ */
 class CompressionMethodComboBox : public QComboBox
 {
 public:
