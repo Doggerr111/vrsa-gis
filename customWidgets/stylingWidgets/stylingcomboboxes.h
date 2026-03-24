459c6b3869bfa0f1169a60287cc1042d97436ab4
Doggerr111 <doggerr111@gmail.com>
2026-03-24 13:26:51 +0300

    refactor: add const to getters, cleanup code, add comments

--- customWidgets/stylingWidgets/stylingcomboboxes.h
+++ customWidgets/stylingWidgets/stylingcomboboxes.h
@@ -1,64 +1,72 @@
-#ifndef LIPPENTYPECOMBOBOX_H
-#define LIPPENTYPECOMBOBOX_H
-
+#ifndef PENTYPECOMBOBOX_H
+#define PENTYPECOMBOBOX_H
 #include <QComboBox>
-namespace vrsa{
-namespace graphics
-{
-    class Symbol;
-}
-}
 
 namespace vrsa{
-namespace common
-{
-    enum class PointSymbolType : int;
+namespace graphics{
+class Symbol;
+}
+namespace common{
+enum class PointSymbolType: int;
 }
 }
 
-
-
+/**
+ * @brief Комбобокс для выбора типа пера (стиля линии)
+ */
 class PenTypeComboBox : public QComboBox
 {
     Q_OBJECT
     using Symbol = vrsa::graphics::Symbol;
 public:
     PenTypeComboBox(const Symbol *symbol, QWidget* parent=nullptr);
-    Qt::PenStyle getStyle();
+    Qt::PenStyle getStyle() const;
     void setStyle(Qt::PenStyle style);
 };
 
+/**
+ * @brief Комбобокс для выбора типа соединений линий
+ */
 class PenJoinComboBox : public QComboBox
 {
     Q_OBJECT
     using Symbol = vrsa::graphics::Symbol;
 public:
     PenJoinComboBox(const Symbol *symbol, QWidget* parent=nullptr);
-    Qt::PenJoinStyle getStyle();
+    Qt::PenJoinStyle getStyle() const;
     void setStyle(Qt::PenJoinStyle style);
 };
 
+/**
+ * @brief Комбобокс для выбора типа кисти (заливки)
+ */
 class BrushTypeComboBox : public QComboBox
 {
     Q_OBJECT
     using Symbol = vrsa::graphics::Symbol;
 public:
     BrushTypeComboBox(const Symbol *symbol, QWidget* parent=nullptr);
-    Qt::BrushStyle getStyle();
+    Qt::BrushStyle getStyle() const;
     void setStyle(Qt::BrushStyle style);
 };
 
+/**
+ * @brief Комбобокс для выбора стиля окончания линии
+ */
 class PenCapComboBox : public QComboBox
 {
     Q_OBJECT
     using Symbol = vrsa::graphics::Symbol;
 public:
     PenCapComboBox(const Symbol* symbol, QWidget* parent=nullptr);
-    Qt::PenCapStyle getStyle();
+    Qt::PenCapStyle getStyle() const;
     void setStyle(Qt::PenCapStyle style);
 };
 
-
+/**
+ * @brief Комбобокс для выбора стиля условного знака точечного символа
+ * @see vrsa::common::PointSymbolType
+ */
 class PointTypeComboBox : public QComboBox
 {
     Q_OBJECT
@@ -67,13 +75,11 @@ class PointTypeComboBox : public QComboBox
 public:
     PointTypeComboBox(const Symbol* symbol, QWidget* parent = nullptr);
     void setPointType(PointType type);
-    PointType getPointType();
-    //PointType getPointType();
+    PointType getPointType() const;
 private:
-    QIcon createIconForType(PointType type);
+    QIcon createIconForType(PointType type) const;
 
 };
 
 
-
-#endif // LIPPENTYPECOMBOBOX_H
+#endif // PENTYPECOMBOBOX_H
